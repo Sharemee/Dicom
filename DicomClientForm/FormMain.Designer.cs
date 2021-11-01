4d6f5709fb2c79f16e07a3e8f135d9315aecb54f
Sunshine <HK_EternalNabula@Outlook.com>
2021-11-01 14:25:10 +0800

    Add test button

--- DicomClientForm/FormMain.Designer.cs
+++ DicomClientForm/FormMain.Designer.cs
@@ -31,6 +31,7 @@ namespace DicomClientForm
         {
             this.BtnLoad = new System.Windows.Forms.Button();
             this.BtnCreateDicom = new System.Windows.Forms.Button();
+            this.BtnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnLoad
@@ -55,11 +56,23 @@ namespace DicomClientForm
             this.BtnCreateDicom.UseVisualStyleBackColor = true;
             this.BtnCreateDicom.Click += new System.EventHandler(this.BtnCreateDicom_Click);
             // 
+            // BtnTest
+            // 
+            this.BtnTest.AutoSize = true;
+            this.BtnTest.Location = new System.Drawing.Point(699, 21);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(75, 23);
+            this.BtnTest.TabIndex = 2;
+            this.BtnTest.Text = "测  试";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnTest);
             this.Controls.Add(this.BtnCreateDicom);
             this.Controls.Add(this.BtnLoad);
             this.Name = "FormMain";
@@ -74,6 +87,7 @@ namespace DicomClientForm
 
         private System.Windows.Forms.Button BtnLoad;
         private System.Windows.Forms.Button BtnCreateDicom;
+        private System.Windows.Forms.Button BtnTest;
     }
 }
 
--- DicomClientForm/FormMain.cs
+++ DicomClientForm/FormMain.cs
@@ -40,16 +40,26 @@ namespace DicomClientForm
 
             var a = df.DataSet.Count;
             var b = df.DataSet.DumpString;
-            Console.WriteLine(b);
+            //Console.WriteLine(b);
 
+            StringBuilder sb = new StringBuilder();
             foreach (var item in df.DataSet)
             {
-                string desc = item.DicomTagDescription;
-                string value = item.GetString(0, string.Empty);
-                Console.WriteLine($"Desc: {desc}\t\t{value}");
+                var tag = item.Tag;
+                string dump = $"({tag.Group},{tag.Element}) {tag.VR} {tag.VM} {tag.VariableName} = {item.GetString(0, string.Empty)}";
+                sb.Append(dump);
 
-                Console.WriteLine(item.Tag.Name);
+                Console.WriteLine(dump);
+
+                //string desc = item.DicomTagDescription;
+                //string value = item.GetString(0, string.Empty);
+                //Console.WriteLine($"Desc: {desc}\t\t{value}");
+
+                //Console.WriteLine(item.Tag.Name);
             }
+
+            int seriesNumber = df.DataSet[DicomTags.SeriesNumber].GetInt32(0, 0);
+            Console.WriteLine("SeriesNumber: " + seriesNumber);
 
         }
 
@@ -58,23 +68,32 @@ namespace DicomClientForm
             //DicomFile df = new DicomFile(files[0].FullName);
             byte[] bytes = new byte[bmp.Width * bmp.Height * 3];
             int n = -1;
-            for (int i = 1; i <= bmp.Height; i++)
+            for (int i = 0; i < bmp.Height; i++)
             {
-                for (int j = 1; j <= bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++)
                 {
-                    Color pixel = bmp.GetPixel(i, j);
+                    Color pixel = bmp.GetPixel(j, i);
                     bytes[++n] = pixel.R;
                     bytes[++n] = pixel.G;
                     bytes[++n] = pixel.B;
                 }
             }
 
-            //DicomFile file = new DicomFile(files[0].FullName);
+            DicomFile file = new DicomFile(files[0].FullName);
+            file.Load();
 
             DicomFile df = new DicomFile();
 
             df.MediaStorageSopClassUid = SopClass.DigitalXRayImageStorageForPresentation.Uid;
             df.TransferSyntax = TransferSyntax.ExplicitVrLittleEndian;
+
+            #region 开始文件Tag内容替换
+            var obj = file.DataSet[DicomTags.PatientsName];
+            //df.DataSet[DicomTags.PatientsName].SetStringValue()
+
+            df.DataSet[DicomTags.SeriesNumber].SetInt32(0, 10003); //file.DataSet[DicomTags.SeriesNumber];
+            df.DataSet[DicomTags.InstanceNumber].SetInt32(0, 1);
+            #endregion
 
             df.DataSet[DicomTags.ImageType].SetStringValue(@"ORIGINAL\PRIMARY");
             df.DataSet[DicomTags.SopClassUid].SetStringValue(SopClass.DigitalXRayImageStorageForPresentation.Uid);
@@ -88,6 +107,15 @@ namespace DicomClientForm
             string fileName = DateTime.UtcNow.Timestamp().ToString() + ".dcm";
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Dicom", fileName);
             df.Save(filePath);
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            DicomFile file = new DicomFile(files[0].FullName);
+            file.Load();
+
+            var obj = file.DataSet[DicomTags.PatientsName];
+
         }
     }
 }
